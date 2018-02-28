1c3b9a22d6779d54d26ea3cb3697ef8f33482258
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-28 04:05:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const driver = {
 function updateDriverWithKeyAndValue(driver, key, value) {
   console.log(driver)
   driver[key] = value
-  console.log('this is a value',value)
+  console.log('this is a value added:',value)
   console.log(driver)
   return driver
 }
