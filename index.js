fdc33bb70fdc68301a30a03aa406a23bb43ca050
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-28 04:13:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   newdriver[key] = value
   return newdriver
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value
+  return driver
+}
